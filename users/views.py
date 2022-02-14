f5a220ffddf01a87ac0dcbcbd82a2202877e01b0
Chen Qiu <qiu.chen1@northeastern.edu>
2022-02-13 20:00:02 -0800

    update user profile with image resize

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are able to login {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -21,7 +21,9 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -32,11 +34,9 @@ def profile(request):
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
 
     return render(request, 'users/profile.html', context)
-
