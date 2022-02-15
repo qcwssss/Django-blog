2deff4866befb69504af93aae2cd4ee82317760f
Chen Qiu <qiu.chen1@northeastern.edu>
2022-02-14 20:56:11 -0800

    update post page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +8,18 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  #<app>/model_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(ListView):
+    model = Post
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': about})
