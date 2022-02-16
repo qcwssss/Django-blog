aebd836418b5c581fb40a6b728168687ffe38c8c
Chen Qiu <qiu.chen1@northeastern.edu>
2022-02-16 09:25:17 -0800

    update reset password via email

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,8 +133,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.envrion.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.envrion.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL  = 'webmaster@my-host.com'
+SERVER_EMAIL    = 'root@my-domain.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
 # Default primary key field type
